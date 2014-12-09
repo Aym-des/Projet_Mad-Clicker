c9b997b5eaa18a0284b2322f4684b49691e3e4df
Jorgebaratamc <baratajorge@hotmail.com>
2014-12-09 15:48:45 +0100

    Update Controleur.java
    Modifications mineures

--- Projet_Mad_Clicker/src/Interface/Controleur.java
+++ Projet_Mad_Clicker/src/Interface/Controleur.java
@@ -3,30 +3,27 @@
  */
 package Interface;
 
-import Créature.Joueur;
-import Créature.Monstre;
+import CrÃ©ature.Joueur;
+import CrÃ©ature.Monstre;
 import Parametres.Parametres;
 
 /**
- * La classe Controleur permet de gérer toutes les interactions dans le jeu
+ * La classe Controleur permet de gÃ©rer toutes les interactions dans le jeu
  */
 public class Controleur {
 
 	public static void /*INT! VOID pour des raisons de test*/ attaque(){
 		/**
-		 * Si les points de vie de la créture sont positifs, décrémente ses points de vie selon les dégâts par clics du joueur
-		 * 
-		 * if (monstre.pdv > 0){
+		* Si les points de vie de la crÃ©ture sont positifs, dÃ©crÃ©mente ses points de vie selon les dÃ©gÃ¢ts par clics du joueur
+		if (monstre.pdv > 0){
 			monstre.pdv = monstre.pdv - joueur.getDpc();
 			return 1;
 		}
 		 */
 		
 		/**
-		 * Si les points de vie de la créatures sont égaux ou inférieurs à 0, retourne 0 pour confirmer la mort
-		 * 
-		 * if (monstre.pdv <= 0){
-		 *
+		* Si les points de vie de la crÃ©atures sont Ã©gaux ou infÃ©rieurs Ã  0, retourne 0 pour confirmer la mort
+		if (monstre.pdv <= 0){
 			niveauSuivant();
 			return 0;
 		}
@@ -34,20 +31,19 @@ public class Controleur {
 	}
 
 	/**
-	 * Méthode exectuée lors du passage au niveau suivant 
+	 * MÃ©thode exectuÃ©e lors du passage au niveau suivant 
 	 */
 	public void niveauSuivant() {
 		/**
-		 * joueur.niveau++;						Augmenter le niveau du joueur courant
-		 * joueur.getPieces() += monstre.gain(); Augmenter le nombre de pièces du joueur courant avec le gain du monstre courant
+		 * joueur.niveau++;				Augmenter le niveau du joueur courant
+		 * joueur.getPieces() += monstre.gain();	Augmenter le nombre de piÃ¨ces du joueur courant avec le gain du monstre courant
 		 * monstre.pdv += getADD_PDV();			Augmenter les points de vie du monstre SUIVANT
 		 */
-
 	}
 
 	/**
-	 * Méthodes pouvoirX effectuant les effets du pouvoir X lorsqu'il est sélectionné
-	 * Mémo: renommer les pouvoirs
+	 * MÃ©thodes pouvoirX effectuant les effets du pouvoir X lorsqu'il est sÃ©lectionnÃ©
+	 * Ne pas renommer les pouvoirs pour des questions de gÃ©nÃ©ricitÃ© !
 	 */
 	public static void pouvoir1() {
 		/**
@@ -70,6 +66,10 @@ public class Controleur {
 		 */
 	}
 
+	/** !!
+	 * N Ã©tant le nombre de pouvoirs disponibles au moment de la version finale !!
+	 *  !!
+	 */
 	public static void pouvoirN() {
 		/**
 		 * joueur.getDpc() += 20000;
@@ -82,10 +82,11 @@ public class Controleur {
 	 */
 	public static void main(String[] args) {
 		/**
-		 * Tant que le nombre de monstres vaincus (dont les points de vie sont tombés à 0) n'est pas atteint, la partie continue
+		 * Tant que le nombre de monstres vaincus (dont les points de vie sont tombÃ©s Ã  0) n'est pas atteint, la partie continue
 		 * 5 ici pour des raisons de test sinon 20 ou 50 
 		 */
 		while (Parametres.nbVictimes < 5){
+			
 
 		}
 	}
