08ccb4b6cd86909443d754ae6686faab0ed433f5
Aym-des <Spartan_Fragueur@hotmail.com>
2014-12-16 03:43:32 +0100

    Update Javadoc

--- Projet_Mad_Clicker/src/principal/package-info.java
+++ Projet_Mad_Clicker/src/principal/package-info.java
@@ -3,6 +3,6 @@
  */
 /**
  * @author Aymeric
- *
+ * Contient la classe Executable qui est la classe contenant la méthode main servant de base pour executer le Jeu ainsi que plusieurs de méthodes principales du jeu
  */
 package principal;
